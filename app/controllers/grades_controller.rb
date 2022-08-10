fc480ac26c05a2946e29288d1d6cdc4f7e304147
Ryan Mottola <rmottola68@gmail.com>
2022-08-10 13:31:12 -0400

    commit tree 5a2a444ad24a665fac077fb53db8bd1a20308504

---
+++ app/controllers/grades_controller.rb
@@ -0,0 +1,4 @@
+class GradesController < ApplicationController
+
+    
+end
---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,13 @@
+class StudentsController < ApplicationController
+
+    def index
+        students = Student.all
+        render json: students
+    end
+
+    def grades
+        grade_list = Student.all.sort {|a, b| b.grade <=> a.grade}
+        render json: grade_list
+    end
+
+end
